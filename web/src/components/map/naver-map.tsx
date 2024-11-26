dba107a4b3a1261b5b2b9a554c44c9f2cde0e445
wonlee6 <wonlee6@gmail.com>
2024-11-26 22:15:49 +0900

    changed eslint -v 9 > 8

--- web/src/components/map/naver-map.tsx
+++ web/src/components/map/naver-map.tsx
@@ -166,16 +166,16 @@ const NaverMap = ({
 export default NaverMap
 
 // 두 지점 간의 거리 계산 (km)
-const calculateDistance = (lat1: number, lon1: number, lat2: number, lon2: number) => {
-  const R = 6371 // 지구의 반지름 (km)
-  const dLat = ((lat2 - lat1) * Math.PI) / 180
-  const dLon = ((lon2 - lon1) * Math.PI) / 180
-  const a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos((lat1 * Math.PI) / 180) *
-      Math.cos((lat2 * Math.PI) / 180) *
-      Math.sin(dLon / 2) *
-      Math.sin(dLon / 2)
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
-  return R * c
-}
+// const _calculateDistance = (lat1: number, lon1: number, lat2: number, lon2: number) => {
+//   const R = 6371 // 지구의 반지름 (km)
+//   const dLat = ((lat2 - lat1) * Math.PI) / 180
+//   const dLon = ((lon2 - lon1) * Math.PI) / 180
+//   const a =
+//     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+//     Math.cos((lat1 * Math.PI) / 180) *
+//       Math.cos((lat2 * Math.PI) / 180) *
+//       Math.sin(dLon / 2) *
+//       Math.sin(dLon / 2)
+//   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+//   return R * c
+// }
